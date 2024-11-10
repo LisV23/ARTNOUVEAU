81c11ec469b3e2eefcc6ce1d94c9053a0084ecbd
LisV23 <paulaesne@gmail.com>
2024-11-10 13:02:53 +0100

    scroll

--- js/script.js
+++ js/script.js
@@ -18,24 +18,6 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-  const navLinks = document.querySelectorAll('a[href^="#"]');
-  
-  navLinks.forEach(link => {
-      link.addEventListener("click", function(e) {
-          e.preventDefault();
-
-          const targetId = this.getAttribute("href").substring(1);
-          const targetElement = document.getElementById(targetId);
-
-          targetElement.scrollIntoView({
-              behavior: "smooth",
-              block: "start"
-          });
-      });
-  });
-});
-
 AOS.init({
     duration: 1000, 
     easing: 'ease',
